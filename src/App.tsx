65c034c74a86662db2cd7d962e8a2f925f5b4d15
wenkey <g.m.venkateshmuni@gmail.com>
2024-09-05 20:00:38 +0200

    refactor: move app routes to ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from "./ui/routes/AppRoutes";
 
 function App() {
   return (
