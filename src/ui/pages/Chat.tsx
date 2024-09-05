210342026df791c2ced766a483a590967ab27e33
wenkey <g.m.venkateshmuni@gmail.com>
2024-09-05 18:25:47 +0200

    feat: Integrate custom components into chat.tsx

--- src/ui/pages/Chat.tsx
+++ src/ui/pages/Chat.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+import ChatHeader from "../components/ChatHeader";
+import CustomMessageInput from "../components/CustomMessageinput";
+import CustomMessageList from "../components/CustomMessageList";
+import { Message } from "../../models/Message";
+
 const Chat = () => {
-  return <h1>Hello chat</h1>;
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handleSendMessage = async (message: string) => {};
+
+  return (
+    <>
+      <div className="chat-ui-container">
+        <ChatHeader />
+        <CustomMessageList messages={messages} />
+        <CustomMessageInput onSendMessage={handleSendMessage} />
+      </div>
+    </>
+  );
 };
 
 export default Chat;
