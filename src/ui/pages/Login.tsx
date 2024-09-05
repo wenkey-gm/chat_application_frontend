d58c0d1b1fff17a8f7245d7b45b287e8a18946f9
wenkey <g.m.venkateshmuni@gmail.com>
2024-09-05 18:03:51 +0200

    feat: Add Login component

--- src/ui/pages/Login.tsx
+++ src/ui/pages/Login.tsx
@@ -1,5 +1,39 @@
-const Login = () => {
-  return <h1>Login</h1>;
+import "../components/css/login.css";
+import React, { useState } from "react";
+import CustomInput from "../components/CustomInput";
+
+const Login: React.FC = () => {
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [userPassword, setUserPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="login-box">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <CustomInput
+          label="Email:"
+          type="email"
+          value={userEmail}
+          onChange={(e) => setUserEmail(e.target.value)}
+          required
+        />
+        <CustomInput
+          label="Password:"
+          type="password"
+          value={userPassword}
+          onChange={(e) => setUserPassword(e.target.value)}
+          required
+        />
+        {error && <div className="error-message">{error}</div>}
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
